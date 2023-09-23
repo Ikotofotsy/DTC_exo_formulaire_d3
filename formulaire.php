f6a3fe53230d9a2b011c4437e42c8f6968249f66
Ikotofotsy <nyrinaratovo@gmail.com>
2023-09-23 20:54:40 +0300

    Request POST

--- formulaire.php
+++ formulaire.php
@@ -1,13 +1,14 @@
+
+<form action="formulaire.php" method="POST">
+    <input type="text" name="nom" placeholder="Nom" required></br>
+    <input type="text" name="prenom" placeholder="Prénoms" required></br>
+    <input type="text" name="telephone" placeholder="Téléphone" required></br>
+    <button type="submit" name="valider">Enregister</button>
+</form>
+
 <?php
     if(isset($_POST['valider']))
         if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['telephone']))
             echo "Nom\t:\t".$_POST['nom']."</br>Prénoms\t:\t".$_POST['prenom']."</br>Téléphone\t:\t".$_POST['telephone'];
 ?>
-
-<form action="formulaire.php" method="POST">
-    <input type="text" name="nom" placeholder="Nom" required>
-    <input type="text" name="prenom" placeholder="Prénoms" required>
-    <input type="text" name="telephone" placeholder="Téléphone" required>
-    <button type="submit" name="valider">Enregister</button>
-</form>
 
